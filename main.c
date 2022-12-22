175dda63d0e4c9dbdba2efbbea35c9d8ebe5167e
the80hz <dnl.vlgv@mail.ru>
2022-12-23 00:26:33 +0400

    fin

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <locale.h>
 
 #define SENTENCE 1000
 #define WORD 50
@@ -18,38 +19,47 @@ char* get_word(char* str, char* word) {
 }
 
 int main() {
+    setlocale(LC_ALL, "Russian");
     const char* punctuation_marks = " .,!?;:-<>[]{}()";
 
-    char text[SENTENCE];
-    fgets(text, SENTENCE, stdin);
+    while(1){
+        char text[SENTENCE];
+        fgets(text, SENTENCE, stdin);
 
-    char* main = text;
-    char word1[WORD];
-    char word1_clean[WORD];
+        char* main = text;
+        char word1[WORD];
+        char word1_clean[WORD];
+        while (*main) {
+            main = get_word(main, word1);
+            strcpy(word1_clean, word1);
+            strcat(word1, " ");
+            while (strchr(punctuation_marks, word1_clean[strlen(word1_clean) - 1]))
+                word1_clean[strlen(word1_clean) - 1] = '\0';
 
-    while (*main) {
-        main = get_word(main, word1);
-        strcpy(word1_clean, word1);
-        strcat(word1, " ");
-        while (strchr(punctuation_marks, word1_clean[strlen(word1_clean) - 1]))
-            word1_clean[strlen(word1_clean) - 1] = '\0';
+            if (strlen(word1_clean) > 0) {
+                char* main2 = main;
+                char word2[WORD];
+                char word2_clean[WORD];
 
-        if (strlen(word1_clean) > 0) {
-            char* main2 = main;
-            char word2[WORD];
-            char word2_clean[WORD];
+                while (*main2) {
+                    main2 = get_word(main2, word2);
+                    strcpy(word2_clean, word2);
+                    strcat(word2, " ");
+                    while (strchr(punctuation_marks, word2_clean[strlen(word2_clean) - 1]))
+                        word2_clean[strlen(word2_clean) - 1] = '\0';
 
-            // проверка на будущее совпадение
-            while (*main2) {
-                main2 = get_word(main2, word2);
-                strcpy(word2_clean, word2);
-                strcat(word2, " ");
-                while (strchr(punctuation_marks, word2_clean[strlen(word2_clean) - 1]))
-                    word2_clean[strlen(word2_clean) - 1] = '\0';
-                if (strlen(word2_clean) > 0)
-                    if (strcmp(word1_clean, word2_clean) == 0);
+                    if (strlen(word2_clean) > 0) {
+                        if (strlen(word1_clean) > strlen(word2_clean)) {
+                            printf("-The sentence is not ordered in ascending order.");
+                            return 0;
+                        }
+                        break;
+                    }
+                }
             }
         }
+        printf("+The sentence is ordered in ascending order.\n");
     }
+
     return 0;
 }
